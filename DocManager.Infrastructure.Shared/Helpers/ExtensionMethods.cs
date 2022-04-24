b10083a5f1d02df42fb1f5d669530c1bd1e2e239
Alex Campos <alexcamposve@gmail.com>
2022-04-24 18:23:53 -0300

    Extension Methods

--- DocManager.Infrastructure.Shared/Helpers/ExtensionMethods.cs
+++ DocManager.Infrastructure.Shared/Helpers/ExtensionMethods.cs
@@ -22,5 +22,85 @@ namespace ServicioTecnico.Infrastructure.Shared.Helpers
             user.Password = null;
             return user;
         }
+
+        public static string GeneratePONumber()
+        {
+            string result;
+            try
+            {
+                result = GenerateNumber("PO");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return result;
+        }
+
+        public static string GenerateSONumber()
+        {
+            string result;
+            try
+            {
+                result = GenerateNumber("SO");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return result;
+        }
+
+        public static string GenerateGRNumber()
+        {
+            string result;
+            try
+            {
+                result = GenerateNumber("GR");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return result;
+        }
+
+        public static string GenerateInvenTranNumber()
+        {
+            string result;
+            try
+            {
+                result = GenerateNumber("TRN");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return result;
+        }
+
+        private static string GenerateNumber(string module)
+        {
+            string result;
+            try
+            {
+                result = Guid.NewGuid().ToString()[..7].ToUpper() + "-" + DateTime.Now.ToString("yyyyMMdd") + "#" + module;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return result;
+        }
     }
 }
