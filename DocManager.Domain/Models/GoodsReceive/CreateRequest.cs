423500f562952eb9953ff01c3187aacfff72ec6c
Alex Campos <alexcamposve@gmail.com>
2022-04-30 14:30:20 -0300

    Goods Receive

---
+++ DocManager.Domain/Models/GoodsReceive/CreateRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicioTecnico.Domain.Models.GoodsReceive
+{
+    public class CreateRequest
+    {
+        public string Description { get; set; }
+        public DateTimeOffset? GoodsReceiveDate { get; set; } = DateTime.Now;
+        public Guid PurchaseOrderId { get; set; }
+    }
+}
---
+++ DocManager.Infrastructure/Interfaces/IPurchaseOrderLineRepositoryAsync.cs
@@ -0,0 +1,14 @@
+﻿using ServicioTecnico.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicioTecnico.Infrastructure.Interfaces
+{
+    public interface IPurchaseOrderLineRepositoryAsync
+    {
+        Task<IEnumerable<PurchaseOrderLine>> GetAllAsync();
+    }
+}
